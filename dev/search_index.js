fface5bbf1e182f48024c2215fddbd24b7cf87f4
Documenter.jl <documenter@juliadocs.github.io>
2022-01-05 05:13:06 +0000

    build based on 48e26af

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpaceGrid","category":"page"},{"location":"#SpaceGrid","page":"Home","title":"SpaceGrid","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpaceGrid.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpaceGrid]","category":"page"}]
+}
