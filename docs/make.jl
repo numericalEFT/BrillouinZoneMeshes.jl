203358f9ad0783c0f0a1704ce81a040fb0b365ab
Kun Chen <chenkun0228@gmail.com>
2022-09-19 17:24:51 -0400

    update docs make

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,12 @@ DocMeta.setdocmeta!(BZMeshes, :DocTestSetup, :(using BZMeshes); recursive=true)
 
 makedocs(;
     modules=[BZMeshes],
-    authors="Tao Wang, Xiansheng Cai",
-    repo="https://github.com/fsxbhyy/BZMeshes.jl/blob/{commit}{path}#{line}",
+    authors="Xiansheng Cai, Tao Wang and Kun Chen",
+    repo="https://github.com/numericaleft/BZMeshes.jl/blob/{commit}{path}#{line}",
     sitename="BZMeshes.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://fsxbhyy.github.io/BZMeshes.jl",
+        canonical="https://numericaleft.github.io/BZMeshes.jl",
         assets=String[]
     ),
     pages=[
