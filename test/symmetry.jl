06a17a4c407c0d725f41259ce530db6756d840c8
Kun Chen <chenkun0228@gmail.com>
2022-10-24 13:30:40 -0400

    fix symmetry.jl

--- test/symmetry.jl
+++ test/symmetry.jl
@@ -10,6 +10,8 @@ lattice = a / 2 * [[0 1 1.0]  # Silicon lattice vectors
     [1 1 0.0]];
 
 positions = [ones(3) / 8, -ones(3) / 8]
+kshift = true
+_kshift = kshift ? [1 // 2, 1 // 2, 1 // 2] : [0, 0, 0]
 
 ######## DFTK interface ########################
 # 2. Select model and basis
@@ -22,14 +24,16 @@ model = model_LDA(lattice, atoms, positions)
 kgrid = [4, 4, 4]     # k-point grid (Regular Monkhorst-Pack grid)
 Ecut = 7              # kinetic energy cutoff
 # Ecut = 190.5u"eV"  # Could also use eV or other energy-compatible units
-basis = PlaneWaveBasis(model; Ecut, kgrid, kshift=[1 // 2, 1 // 2, 1 // 2])
+basis = PlaneWaveBasis(model; Ecut, kgrid, kshift=_kshift)
 
 br = BrillouinZoneMeshes.Model.Brillouin(lattice=austrip.(lattice), atoms=[1, 1], positions=positions)
-brmesh = BrillouinZoneMeshes.UniformBZMesh(br=br, size=tuple(kgrid...), shift=1 // 2)
+brmesh = BrillouinZoneMeshes.UniformBZMesh(br=br, size=tuple(kgrid...), shift=_kshift[1])
 
-meshmap = BrillouinZoneMeshes.reduced_uniform_meshmap(br, kgrid_size=kgrid, kshift=true)
+meshmap = BrillouinZoneMeshes.reduced_uniform_meshmap(br, kgrid_size=kgrid, kshift=kshift)
 println(meshmap.irreducible_indices)
-for ind in meshmap.irreducible_indices
+for (i, ind) in enumerate(meshmap.irreducible_indices)
     # println(AbstractMeshes._ind2inds(tuple(kgrid...), ind))
-    println(brmesh[AbstractMeshes._ind2inds(tuple(kgrid...), ind)...])
+    _kvec = DFTK.recip_vector_red_to_cart(model, basis.kcoords_global[i])
+    kvec = brmesh[AbstractMeshes._ind2inds(tuple(kgrid...), ind)...]
+    println("$kvec vs $_kvec")
 end
