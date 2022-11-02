9820ff54ba4a34553eec0deaf0b7d58566328f00
Xiansheng Cai <iintsjds@gmail.com>
2022-11-02 17:29:31 -0400

    wip

--- example/polarmesh_generator.jl
+++ example/polarmesh_generator.jl
@@ -38,15 +38,25 @@ function kF_densed_kgrids(; dispersion,
     return grids
 end
 
+
+function BZMeshes.PolarMesh(; dispersion, anglemesh, br, kmax,
+    kwargs...)
+    bound = [0.0, kmax]
+    grids = kF_densed_kgrids(dispersion=dispersion, anglemesh=anglemesh, bound=bound, kwargs...)
+    cm = CompositeMesh(anglemesh, grids)
+    pm = PolarMesh(br, cm)
+    return pm
+end
+
 @testset "PolarMesh Generator" begin
-    @testset "Find k_F" begin
+    @testset "2D" begin
         # given dispersion function accept a k in cartesian
         # goal is to find k_F at direction specified by angle
         dispersion(k) = dot(k, k) - 1.0
 
         # 2d
         N = 10
-        bound = [0, 2π]
+        bound = [-π, π]
         theta = SimpleGrid.Uniform(bound, N; isperiodic=true)
 
         k_F_previous = 0.0
@@ -58,14 +68,46 @@ end
             k_F_previous = k_F
         end
 
-        grids = kF_densed_kgrids(dispersion=dispersion, anglemesh=theta,
-            bound=[0.0, 2.0])
-        cm = CompositeMesh(theta, grids)
+        # grids = kF_densed_kgrids(dispersion=dispersion, anglemesh=theta,
+        #    bound=[0.0, 2.0])
+        # cm = CompositeMesh(theta, grids)
         DIM = 2
         lattice = Matrix([1.0 0; 0 1]')
         br = BZMeshes.Brillouin(lattice=lattice)
 
-        pm = PolarMesh(br, cm)
+        pm = PolarMesh(dispersion=dispersion, anglemesh=theta, br=br, kmax=2.0)
         @test AbstractMeshes.volume(pm) ≈ 4π
+
+    end
+
+    @testset "3D" begin
+        dispersion(k) = dot(k, k) - 1.0
+
+        N = 6
+        bound = [-π, π]
+        phi = SimpleGrid.Uniform(bound, N; isperiodic=true)
+
+        N = 4
+        bound = [-π / 2, π / 2]
+        theta = SimpleGrid.Uniform(bound, N; isperiodic=true)
+
+        am = CompositeMesh(phi, [theta for i in 1:length(phi)])
+
+        k_F_previous = 0.0
+        for ap in am
+            f(k) = dispersion(BZMeshes._spherical2cart(Spherical(k, ap...)))
+            k_F = find_zero(f, k_F_previous)
+            @test k_F ≈ 1.0
+            @test find_kFermi(dispersion, ap; kinit=k_F_previous) ≈ 1.0
+            k_F_previous = k_F
+        end
+
+        DIM = 3
+        lattice = Matrix([1.0 1.0 0; 1 0 1; 0 1 1]')
+        br = BZMeshes.Brillouin(lattice=lattice)
+
+        pm = PolarMesh(dispersion=dispersion, anglemesh=am, br=br, kmax=2.0)
+        @test AbstractMeshes.volume(pm) ≈ 32π / 3
+
     end
 end
