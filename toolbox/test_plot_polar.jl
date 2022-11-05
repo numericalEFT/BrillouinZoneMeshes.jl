ee232d661672d975c0de76025a0cc0d5f641d439
Xiansheng Cai <iintsjds@gmail.com>
2022-11-05 10:30:26 -0400

    wip

--- toolbox/test_plot_polar.jl
+++ toolbox/test_plot_polar.jl
@@ -106,22 +106,25 @@ include("../example/polarmesh_generator.jl")
 
 # Wigner-Seitz cells visualization
 # 2D
-#N, DIM = 4, 2
-#origin = [0.0 0.0]
+# N, DIM = 4, 2
+# origin = [0.0 0.0]
 # lattice = Matrix([2 0; 1 sqrt(3)]')
-#br = BZMeshes.Brillouin(lattice = lattice)
+# br = BZMeshes.Brillouin(lattice=lattice)
 
-# latvec = [1 0; 0 1]'
+# lattice = [1 0; 0 1]'
+# atoms = [1]
+# positions = [zeros(2)]
+
 # 3D
 
-lattice = [[0 0.5 0.5]; [0.5 0 0.5]; [0.5 0.5 0.0]]
-atoms = [1, 1]
-positions = [ones(3) / 8, -ones(3) / 8]
+# lattice = [[0 0.5 0.5]; [0.5 0 0.5]; [0.5 0.5 0.0]]
+# atoms = [1, 1]
+# positions = [ones(3) / 8, -ones(3) / 8]
 
-#lattice = [[1.0 0.0 0.0]; [0.0 1.0 0.0]; [0.0 0.0 1.0]]
-#atoms = [1]
-#positions = [zeros(3)]
-#positions = [zeros(2)]
+lattice = [[1.0 0.0 0.0]; [0.0 1.0 0.0]; [0.0 0.0 1.0]]
+atoms = [1]
+positions = [zeros(3)]
+
 
 atom_pos = hvcat(size(positions, 1), positions...)
 ibzformat = "convex hull"
