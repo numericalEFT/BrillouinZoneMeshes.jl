d79995b1ce413d8b72b5061e346c1c5a434f98a0
Kun Chen <chenkun0228@gmail.com>
2022-11-12 23:06:25 -0500

    add multi-dimensional fourier interpolation

--- example/sodium/interpolatin.jl
+++ example/sodium/interpolatin.jl
@@ -1,56 +1,10 @@
-using Plots
+# using Plots
 using FFTW
 
-dim = 2
-
-Nx = 30;
-Nxfine = 60;
-x = collect((-Nx/2:Nx/2-1) / Nx);
-y = z = x
-xfine = collect((-Nxfine/2:Nxfine/2-1) / Nxfine);
-yfine = zfine = xfine
-
-if dim == 1
-    f = exp.(sin.(2π * x))
-    fine = exp.(sin.(2π * xfine))
-
-elseif dim == 2
-    f = zeros(Float64, Nx, Nx)
-    fine = zeros(Float64, Nxfine, Nxfine)
-    for xi in 1:Nx
-        for yi in 1:Nx
-            f[xi, yi] = exp(sin(2π * x[xi]) + cos(2π * y[yi]))
-        end
-    end
-    for xi in 1:Nxfine
-        for yi in 1:Nxfine
-            fine[xi, yi] = exp(sin(2π * xfine[xi]) + cos(2π * yfine[yi]))
-        end
-    end
-else
-    error("not implemented!")
-end
-
-# ft = fftshift(fft(f))
-
-# Npad = floor(Int64, Ni / 2 - N / 2)
-# ft_pad = [zeros(Npad); ft; zeros(Npad)];
-# f_interp = real(ifft(fftshift(ft_pad))) * Ni / N;
-
-
-# function interpolate(f::AbstractVector, Ni::Int64)
-#     N = length(f)
-#     Npad = floor(Int64, Ni / 2 - N / 2)
-#     ft = fftshift(fft(fftshift(f))) # first shift t0 0: N-1, then fft
-#     ft_pad = [zeros(Npad); ft; zeros(Npad)]
-#     f_interp = real(fftshift(ifft(fftshift(ft_pad)))) * Ni / N
-#     xi = collect((-Ni/2:Ni/2-1) / Ni)
-#     return f_interp, xi
-# end
 
 # assume f(x) where x = -N/2:N/2-1
 # return f(y) where y = -Ni/2:Ni/2-1
-function interpolate(f::AbstractArray{T,D}, Ni::Int64) where {T,D}
+function fourier_interpolate(f::AbstractArray{T,D}, Ni::Int64) where {T,D}
     N = size(f, 1)
     @assert all(x -> x == N, size(f))
     dim = [i for i in 1:D]
@@ -75,25 +29,73 @@ function interpolate(f::AbstractArray{T,D}, Ni::Int64) where {T,D}
     sft_pad = fftshift(ft_pad, dim) # shift to 0: Ni-1
     isft_pad = ifft(sft_pad, dim) # ifft
     f_interp = real(fftshift(isft_pad)) * (Ni / N)^D #shift to Ni/2: Ni/2-1
-    xi = collect((-Ni/2:Ni/2-1) / Ni)
-    return f_interp, xi
+    return f_interp
 end
 
-if dim == 1
-    f_interp, xi = interpolate(f, 300)
-    plot(x, f, label="Original samples", markershape=:circle)
-    plot!(xi, f_interp, label="Interpolated values")
-elseif dim == 2
-    # f_interp, xi = interpolate(f, 300)
-    # # p = plot(st= :surface, camera = (30, 60), size=(600, 600))
-    # # plot!(p, x, y, f, st= :surface)
-    # # plot!(p, xi, xi, f_interp, st= :surface)
+if abspath(PROGRAM_FILE) == @__FILE__
 
-    f_interp, xi = interpolate(f, Nxfine)
-    println(maximum(abs.(f_interp - fine)))
 
-    plot(x, f[1, :], label="Original samples", markershape=:circle)
-    plot!(xi, f_interp[1, :], label="Interpolated values")
-else
-    error("not implemented!")
+    Nx = 30
+    Nxfine = 100
+    x = collect((-Nx/2:Nx/2-1) / Nx)
+    y = z = x
+    xfine = collect((-Nxfine/2:Nxfine/2-1) / Nxfine)
+    yfine = zfine = xfine
+
+    ##### DIM =1 #########################
+    f = exp.(sin.(2π * x))
+    fine = exp.(sin.(2π * xfine))
+
+    f_interp, xi = fourier_interpolate(f, Nxfine)
+    println("DIM = 1 test: ", maximum(abs.(f_interp - fine)))
+
+    # plot(x, f, label="Original samples", markershape=:circle)
+    # plot!(xi, f_interp, label="Interpolated values")
+
+    ##### DIM =2 #########################
+    f = zeros(Float64, Nx, Nx)
+    fine = zeros(Float64, Nxfine, Nxfine)
+    f2(x, y) = exp(sin(2π * x) + cos(2π * y))
+    for xi in 1:Nx
+        for yi in 1:Nx
+            f[xi, yi] = f2(x[xi], y[yi])
+        end
+    end
+    for xi in 1:Nxfine
+        for yi in 1:Nxfine
+            fine[xi, yi] = f2(xfine[xi], yfine[yi])
+        end
+    end
+
+    f_interp, xi = fourier_interpolate(f, Nxfine)
+    println("DIM = 2 test: ", maximum(abs.(f_interp - fine)))
+
+    # plot(x, f[1, :], label="Original samples", markershape=:circle)
+    # plot!(xfine, f_interp[1, :], label="Interpolated values")
+
+    ##### DIM =3 #########################
+    f = zeros(Float64, Nx, Nx, Nx)
+    fine = zeros(Float64, Nxfine, Nxfine, Nxfine)
+    f3(x, y, z) = exp(sin(2π * x) + cos(2π * y) + sin(2π * z))
+    for xi in 1:Nx
+        for yi in 1:Nx
+            for zi in 1:Nx
+                f[xi, yi, zi] = f3(x[xi], y[yi], z[zi])
+            end
+        end
+    end
+    for xi in 1:Nxfine
+        for yi in 1:Nxfine
+            for zi in 1:Nxfine
+                fine[xi, yi, zi] = f3(xfine[xi], yfine[yi], zfine[zi])
+            end
+        end
+    end
+
+    f_interp, xi = fourier_interpolate(f, Nxfine)
+    println("DIM = 3 test: ", maximum(abs.(f_interp - fine)))
+
+    # plot(x, f[1, 1, :], label="Original samples", markershape=:circle)
+    # plot!(xfine, f_interp[1, 1, :], label="Interpolated values")
+
 end
