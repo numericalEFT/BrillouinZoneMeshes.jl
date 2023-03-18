96c089d2a89812c7f27231515773c819d33d46fb
Xiansheng Cai <iintsjds@gmail.com>
2023-03-17 22:17:40 -0400

    wip

--- src/AbstractMeshes.jl
+++ src/AbstractMeshes.jl
@@ -1,3 +1,13 @@
+"""
+    module AbstractMeshes
+
+This sub-module defines abstract type "AbstractMesh", 
+from which all concrete types of meshes in this package derive. 
+All functions expected by a sub-type of "AbstractMesh" are defined in this file, 
+including AbstractArray interface requirements and functions like "locate" and "volume".
+If the implementation of a function is type-specific, 
+the function defined in this file will return error.
+"""
 module AbstractMeshes
 
 # define interface of AbstractMesh
